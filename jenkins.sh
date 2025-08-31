61a82b8d0aeca336f2eac45ed16d0b68434f4494
vikash-bunkar <vikashbunkar123@gmail.com>
2025-08-31 19:56:07 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+sudo yum update -y
+sudo wget -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo yum install java-17-amajon-corretto -y
+sudo yum install jenkins git -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo mkdir -p /var/tmp_disk
+sudo chmod 1777 /var/tmp_disk
+sudo mount --bind /var/tmp_disk /tmp
+echo "/var/tmp_disk /tmp none 0 0" | sudo tee -a /etc/fstab
+sudo systemctl mask tmp.mount
+df -h /tmp
+sudo systemctl restart jenkins
+
