6b609b7c9eae4c702450bad48e75272c86c8a489
vikash-bunkar <vikashbunkar123@gmail.com>
2025-08-31 20:18:45 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo yum update -y
-sudo wget -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 sudo yum install java-17-amajon-corretto -y
